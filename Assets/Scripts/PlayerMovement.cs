edd3e32b570e21a6100e7619dedf5317b5fc0789
Daniel Barella <dan.barella@gmail.com>
2012-10-09 21:55:59 -0400

    Fixed PlayerMovement boundaries.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,18 +7,26 @@ public class PlayerMovement : MonoBehaviour {
 	bool canMove;
 	float speed = 5.0f;
 	
+	//Movement Boundaries
+	private float max_y;
+	private float min_y;
+	float buffer = -2;
+	
 	// Use this for initialization
 	void Start () {
 		canMove = true;
+		
+		//Define movement boundaries
+		max_y = Camera.main.orthographicSize + buffer;
+		min_y = - max_y;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(canMove) {
-			if(Input.GetKey(KeyCode.UpArrow)) { //If the player presses the 'up' key
-				Debug.Log("Moving up.");
+			if(Input.GetKey(KeyCode.UpArrow) && (transform.position.y < max_y)) { //If the player presses the 'up' key
 				transform.Translate(speed * Vector3.up * Time.deltaTime); //Move the player up
-			} if(Input.GetKey(KeyCode.DownArrow)) { //
+			} if(Input.GetKey(KeyCode.DownArrow) && (transform.position.y > min_y)) { //
 				transform.Translate(speed * -Vector3.up * Time.deltaTime); //Move the player down
 			}
 		}
